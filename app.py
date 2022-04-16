cd37e840b28407fc473b6c01bbb5c72a1de3d4ee
handrahilman <handrahilman@gmail.com>
2022-04-16 10:07:34 +0700

    add function serang

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 class Hero:
     jumlah = 0
     
-    def __init__(self,nama,health,attack,defense,):
-        self.nama = nama
+    def __init__(self,name,health,attack,defense,):
+        self.name = name
         self.health = health
         self.attack = attack
         self.defense = defense
         Hero.jumlah += 1
 
     def get_nama(self):
-        print("nama :{}".format(self.nama))
+        print("name :{}".format(self.name))
     def get_health(self):
         print("health :{}".format(self.health))
     def get_attack(self):
         print("attack :{}".format(self.attack))
     def get_defense(self):
         print("defense :{}".format(self.defense))
+
+    def serang(self,lawan):
+        print(f"{self.nama} menyerang {lawan.nama}")
+        lawan.health -= (self.attack - lawan.defense)
+        print(f"serangan terasa {self.attack}")
+        print(f"sisa nyawa : {lawan.health}")
+        if lawan.health < 0:
+            print("Hero sudah mati")
+        else:
+            pass
+    
